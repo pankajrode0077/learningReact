1945c544eb6a72c12227b9eaca9709a98837f00a
Prateek Upadhyay <prateek.upadhyay@niceincontact.com>
2021-07-08 16:42:09 +0530

    demo flow

--- src/App.js
+++ src/App.js
@@ -116,163 +116,164 @@ const updateView = (lbl)=>{
   }
 
   return (
-    <>
-    {/* <DemoComp1></DemoComp1> */}
-    {/* <DemoHomeComp></DemoHomeComp> */}
-      {step === 0 ?
-        <div className={`App base ${state ? "disable-area" : ""}`}>
-          <button className="addScreen" onClick={() => setScreen(screen + 1)}>Add New Screen</button>
-          <button className="importEnv" onClick={callupload}>Import Config File</button>
-          <input id="up" className="upload" type="file"></input>
-          <div className="vl"><label className="paddingleft">Screen 1</label></div>
-          {screen > 0 && <div className="v2"><label className="paddingleft">Screen 2</label></div>}
-          {screen > 1 && <div className="v3"><label className="paddingleft">Screen 3</label></div>}
-          <div id="mySidenav" className="sidenav">
-            {login.disp && <p>Login </p>}
-            <Draggable
-              key="1"
-              defaultClassName="limitWidth"
-              defaultPosition={login.pos}
-              onStop={(e, data) => {
-                updatePos(data, 1, { type: 'login' });
-              }}
-              onStart={() => {
-                updateView('login');
-              }}
-            >
-              <div>
-                <Login expanded={login.exp}></Login>
-              </div>
-            </Draggable>
-            {navbar.disp && <p>NavBar </p>}
-            <Draggable
-              key="2"
-              defaultClassName="limitWidth"
-              defaultPosition={navbar.pos}
-              onStop={(e, data) => {
-                updatePos(data, 1, { type: 'navbar' });
-              }}
-              onStart={() => {
-                updateView('navbar');
-              }}
-            >
-              <div>
-                <Navbar expanded={navbar.exp}></Navbar>
-              </div>
-            </Draggable>
-            {agentstatus.disp && <p>Agent Status </p>}
-            <Draggable
-              key="3"
-              defaultClassName="limitWidth"
-              defaultPosition={agentstatus.pos}
-              onStop={(e, data) => {
-                updatePos(data, 1, { type: 'agentstatus' });
-              }}
-              onStart={() => {
-                updateView('agentstatus');
-              }}
-            >
-              <div>
-                <AgentStatus expanded={agentstatus.exp}></AgentStatus>
-              </div>
-            </Draggable>
-            {sidemenu.disp && <p>Side Menu </p>}
-            <Draggable
-              key="4"
-              defaultClassName="limitWidth"
-              defaultPosition={sidemenu.pos}
-              onStop={(e, data) => {
-                updatePos(data, 1, { type: 'sidemenu' });
-              }}
-              onStart={() => {
-                updateView('sidemenu');
-              }}
-            >
-              <div>
-                <SideMenu expanded={sidemenu.exp}></SideMenu>
-              </div>
-            </Draggable>
-            {stickyNotes.disp && <p>Sticky Notes </p>}
-            <Draggable
-              key="6"
-              defaultClassName="limitWidth"
-              defaultPosition={stickyNotes.pos}
-              onStop={(e, data) => {
-                updatePos(data, 1, { type: 'stickyNotes' });
-              }}
-              onStart={() => {
-                updateView('stickyNotes');
-              }}
-            >
-              <div>
-                <StickyNotes expanded={stickyNotes.exp}></StickyNotes>
-              </div>
-            </Draggable>
-            <p>Digital </p>
-            <p>Email </p>
-            <p>SMS </p>
-            <p>Commitments </p>
-            {dialpad.disp && <p>DialPad </p>}
-            <Draggable
-              key="5"
-              defaultClassName="limitWidth"
-              defaultPosition={dialpad.pos}
-              onStop={(e, data) => {
-                updatePos(data, 1, { type: 'dialpad' });
-              }}
-              onStart={() => {
-                updateView('dialpad');
-              }}
-            >
-              <div>
-                <Dialpad expanded={dialpad.exp}></Dialpad>
-              </div>
-            </Draggable>
-            {weather.disp && <p>Weather Widget</p>}
-            <Draggable
-              key="7"
-              defaultClassName="limitWidth"
-              defaultPosition={weather.pos}
-              onStop={(e, data) => {
-                updatePos(data, 1, { type: 'weather' });
-              }}
-              onStart={() => {
-                updateView('weather');
-              }}
-            >
-              <div>
-                <Weather expanded={weather.exp}></Weather>
-              </div>
-            </Draggable>
-            <button className="export" onClick={move}>Export App</button>
-            <button className="dropdown" onClick={() => { setopenTheme(openTheme?false :true) }} >Themes</button>
-            <div className="dropdowns">
-              {openTheme && <div id="myDropdown" class="dropdown-content">
-                <label className="darkTheme thmtxt" onClick={() => { setopenTheme(false) }}>Dark</label><br/>
-                <label className="lightTheme thmtxt" onClick={() => { setopenTheme(false) }}>Light</label><br/>
-                <label className="orangeTheme thmtxt" onClick={() => { setopenTheme(false) }}>Orange</label><br/>
-                <label className="blueTheme thmtxt" onClick={() => { setopenTheme(false) }}>Blue</label><br/>
-              </div>}
-            </div>
-          </div>
-        </div>
+    <DemoComp1></DemoComp1> 
+    // <>
+    // {/* <DemoComp1></DemoComp1> */}
+    // {/* <DemoHomeComp></DemoHomeComp> */}
+    //   {step === 0 ?
+    //     <div className={`App base ${state ? "disable-area" : ""}`}>
+    //       <button className="addScreen" onClick={() => setScreen(screen + 1)}>Add New Screen</button>
+    //       <button className="importEnv" onClick={callupload}>Import Config File</button>
+    //       <input id="up" className="upload" type="file"></input>
+    //       <div className="vl"><label className="paddingleft">Screen 1</label></div>
+    //       {screen > 0 && <div className="v2"><label className="paddingleft">Screen 2</label></div>}
+    //       {screen > 1 && <div className="v3"><label className="paddingleft">Screen 3</label></div>}
+    //       <div id="mySidenav" className="sidenav">
+    //         {login.disp && <p>Login </p>}
+    //         <Draggable
+    //           key="1"
+    //           defaultClassName="limitWidth"
+    //           defaultPosition={login.pos}
+    //           onStop={(e, data) => {
+    //             updatePos(data, 1, { type: 'login' });
+    //           }}
+    //           onStart={() => {
+    //             updateView('login');
+    //           }}
+    //         >
+    //           <div>
+    //             <Login expanded={login.exp}></Login>
+    //           </div>
+    //         </Draggable>
+    //         {navbar.disp && <p>NavBar </p>}
+    //         <Draggable
+    //           key="2"
+    //           defaultClassName="limitWidth"
+    //           defaultPosition={navbar.pos}
+    //           onStop={(e, data) => {
+    //             updatePos(data, 1, { type: 'navbar' });
+    //           }}
+    //           onStart={() => {
+    //             updateView('navbar');
+    //           }}
+    //         >
+    //           <div>
+    //             <Navbar expanded={navbar.exp}></Navbar>
+    //           </div>
+    //         </Draggable>
+    //         {agentstatus.disp && <p>Agent Status </p>}
+    //         <Draggable
+    //           key="3"
+    //           defaultClassName="limitWidth"
+    //           defaultPosition={agentstatus.pos}
+    //           onStop={(e, data) => {
+    //             updatePos(data, 1, { type: 'agentstatus' });
+    //           }}
+    //           onStart={() => {
+    //             updateView('agentstatus');
+    //           }}
+    //         >
+    //           <div>
+    //             <AgentStatus expanded={agentstatus.exp}></AgentStatus>
+    //           </div>
+    //         </Draggable>
+    //         {sidemenu.disp && <p>Side Menu </p>}
+    //         <Draggable
+    //           key="4"
+    //           defaultClassName="limitWidth"
+    //           defaultPosition={sidemenu.pos}
+    //           onStop={(e, data) => {
+    //             updatePos(data, 1, { type: 'sidemenu' });
+    //           }}
+    //           onStart={() => {
+    //             updateView('sidemenu');
+    //           }}
+    //         >
+    //           <div>
+    //             <SideMenu expanded={sidemenu.exp}></SideMenu>
+    //           </div>
+    //         </Draggable>
+    //         {stickyNotes.disp && <p>Sticky Notes </p>}
+    //         <Draggable
+    //           key="6"
+    //           defaultClassName="limitWidth"
+    //           defaultPosition={stickyNotes.pos}
+    //           onStop={(e, data) => {
+    //             updatePos(data, 1, { type: 'stickyNotes' });
+    //           }}
+    //           onStart={() => {
+    //             updateView('stickyNotes');
+    //           }}
+    //         >
+    //           <div>
+    //             <StickyNotes expanded={stickyNotes.exp}></StickyNotes>
+    //           </div>
+    //         </Draggable>
+    //         <p>Digital </p>
+    //         <p>Email </p>
+    //         <p>SMS </p>
+    //         <p>Commitments </p>
+    //         {dialpad.disp && <p>DialPad </p>}
+    //         <Draggable
+    //           key="5"
+    //           defaultClassName="limitWidth"
+    //           defaultPosition={dialpad.pos}
+    //           onStop={(e, data) => {
+    //             updatePos(data, 1, { type: 'dialpad' });
+    //           }}
+    //           onStart={() => {
+    //             updateView('dialpad');
+    //           }}
+    //         >
+    //           <div>
+    //             <Dialpad expanded={dialpad.exp}></Dialpad>
+    //           </div>
+    //         </Draggable>
+    //         {weather.disp && <p>Weather Widget</p>}
+    //         <Draggable
+    //           key="7"
+    //           defaultClassName="limitWidth"
+    //           defaultPosition={weather.pos}
+    //           onStop={(e, data) => {
+    //             updatePos(data, 1, { type: 'weather' });
+    //           }}
+    //           onStart={() => {
+    //             updateView('weather');
+    //           }}
+    //         >
+    //           <div>
+    //             <Weather expanded={weather.exp}></Weather>
+    //           </div>
+    //         </Draggable>
+    //         <button className="export" onClick={move}>Export App</button>
+    //         <button className="dropdown" onClick={() => { setopenTheme(openTheme?false :true) }} >Themes</button>
+    //         <div className="dropdowns">
+    //           {openTheme && <div id="myDropdown" class="dropdown-content">
+    //             <label className="darkTheme thmtxt" onClick={() => { setopenTheme(false) }}>Dark</label><br/>
+    //             <label className="lightTheme thmtxt" onClick={() => { setopenTheme(false) }}>Light</label><br/>
+    //             <label className="orangeTheme thmtxt" onClick={() => { setopenTheme(false) }}>Orange</label><br/>
+    //             <label className="blueTheme thmtxt" onClick={() => { setopenTheme(false) }}>Blue</label><br/>
+    //           </div>}
+    //         </div>
+    //       </div>
+    //     </div>
 
-        : step === 1 ? <DemoComp1 /> : step === 2 ? <DemoHomeComp /> : ""
+    //     : step === 1 ? <DemoComp1 /> : step === 2 ? <DemoHomeComp /> : ""
 
-      }
-      <div>
-        {state && (
-          <>
-            <div id="myProgress" className='progress-bar' onClick={move}>
-              <div id="myBar" style={{ width: `${progWidth}%` }}>{progWidth}%
-              </div>
-            </div>
-            <span className="progress-msg-position"> Exporting project...</span>
-          </>
-        )}
-      </div>
+    //   }
+    //   <div>
+    //     {state && (
+    //       <>
+    //         <div id="myProgress" className='progress-bar' onClick={move}>
+    //           <div id="myBar" style={{ width: `${progWidth}%` }}>{progWidth}%
+    //           </div>
+    //         </div>
+    //         <span className="progress-msg-position"> Exporting project...</span>
+    //       </>
+    //     )}
+    //   </div>
 
-    </>
+    // </>
   
   );
 }
--- src/DemoComp1.js
+++ src/DemoComp1.js
@@ -1,61 +1,123 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Login from "./Login";
+import AgentStatusImg from '../src/assets/Home/AgentStatus.png';
+import AgentDispImg from '../src/assets/dialpad/dispo.png';
+import StickyNotes from "./StickyNotes";
 
 function DemoComp1() {
     const [step, setStep] = useState(1);
+    const [shLoad, setSHLoad] = useState(false);
+    const keypressed = (e) => {
+        console.log(e.key);
+        switch (e.key) {
+            case 'l':
+                setStep(1);
+                break;
+            case 'd':
+                setStep(2);
+                break;
+            case 'c':
+                setStep(6);
+                break;
+            case 's':
+                setSHLoad(true);
+                setTimeout(() => { setStep(7); setSHLoad(false); }, 1000)
+                break;
+            case 'q':
+                setSHLoad(true);
+                setTimeout(() => { setStep(step + 1); setSHLoad(false); }, 1000)
+                break;
+            default:
+                break;
+        }
+    }
     return (<>
-        {
+        <input style={{ border: "none", "font-size":"5px "}} onKeyPress={keypressed}></input>
+        {shLoad === true ?
+            <div>
+                <img alt="loading" class="loader-spinner"
+                    src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/loader-spinner.png'></img>
+            </div>
+
+            :
             step === 1 ?
                 <div >
-                    <Login expanded={true} changestep={()=>setStep(2)}></Login>
+                    <Login expanded={true} changestep={() => setStep(2)}></Login>
                 </div> :
-            step === 2 ?
-            <div >
-                <img alt="loading"
-                    onClick={()=>setStep(3)}
-                    height="500px"
-                    width="70%"
-                    src={require('./assets/dialpad/OBCall1.png')}></img>
-            </div> :
-            step === 3 ?
-            <div >
-                <img alt="loading"
-                    onClick={()=>setStep(4)}
-                    height="500px"
-                    width="70%"
-                    src={require('./assets/dialpad/OBCall2.png')}></img>
-            </div> :
-            step === 4 ?
-            <div >
-                <img alt="loading"
-                    onClick={()=>setStep(5)}
-                    height="500px"
-                    width="70%"
-                    src={require('./assets/dialpad/OBCall3.png')}></img>
-            </div> :
-            step === 5 ?
-            <div >
-                <img alt="loading"
-                    onClick={()=>setStep(6)}
-                    height="500px"
-                    width="70%"
-                    src={require('./assets/dialpad/OBCall4.png')}></img>
-            </div> :
-            step === 6 ?
-            <div >
-                <img alt="loading"
-                    onClick={()=>setStep(7)}
-                    height="500px"
-                    width="70%"
-                    src={require('./assets/dialpad/OBCall5.png')}></img>
-            </div>
-            
-            
-            : ""     
-            
+                step === 2 ?
+                    <div style={{ display: "grid" }}>
+                        <img alt="loading" width="300px" src={AgentStatusImg}></img>
+                        <img alt="loading"
+                            onClick={() => setStep(3)}
+                            height="500px"
+                            width="300px"
+                            src={require('./assets/dialpad/OBCall1.png')}></img>
+                    </div> :
+                    step === 3 ?
+                        <div style={{ display: "grid" }}>
+                            <img alt="loading" width="300px" src={AgentStatusImg}></img>
+                            <img alt="loading"
+                                onClick={() => setStep(4)}
+                                height="500px"
+                                width="300px"
+                                src={require('./assets/dialpad/OBCall2.png')}></img>
+                        </div> :
+                        step === 4 ?
+                            <div style={{ display: "grid" }}>
+                                <img alt="loading" width="300px" src={AgentStatusImg}></img>
+                                <img alt="loading"
+                                    onClick={() => setStep(5)}
+                                    height="500px"
+                                    width="300px"
+                                    src={require('./assets/dialpad/OBCall3.png')}></img>
+                            </div> :
+                            step === 5 ?
+                                <div style={{ display: "grid" }}>
+                                    <img alt="loading" width="300px" src={AgentStatusImg}></img>
+                                    <img alt="loading"
+                                        onClick={() => {
+                                            setSHLoad(true);
+                                            setTimeout(() => { setStep(6); setSHLoad(false); }, 1000)
+                                        }}
+                                        height="500px"
+                                        width="300px"
+                                        src={require('./assets/dialpad/OBCall4.png')}></img>
+                                </div> :
+
+                                step === 6 ?
+                                    <div style={{ display: "grid" }} >
+                                        <img alt="loading" width="300px" src={AgentStatusImg}></img>
+                                        <img alt="loading"
+                                            onClick={() => {
+                                                setSHLoad(true);
+                                                setTimeout(() => { setStep(8); setSHLoad(false); }, 1000)
+                                            }}
+                                            height="500px"
+                                            width="300px"
+                                            src={require('./assets/dialpad/OBCall5.png')}></img>
+                                    </div> :
+                                    step === 7 ?
+                                        <div style={{ display: "grid" }} >
+                                            <img alt="loading" width="300px" src={AgentStatusImg}></img>
+                                            <StickyNotes expanded={true}></StickyNotes>
+                                        </div> :
+                                        step === 8 ?
+                                            <div style={{ display: "grid" }} >
+                                                <img alt="loading" width="300px" src={AgentStatusImg}></img>
+                                                <img alt="loading"
+                                                    onClick={() => {
+                                                        setSHLoad(true);
+                                                        setTimeout(() => { setStep(6); setSHLoad(false); }, 1000)
+                                                    }}
+                                                    height="500px"
+                                                    width="300px"
+                                                    src={AgentDispImg}></img>
+                                            </div> :
+                                            ""
+
         }
-            </>);
+    </>);
 }
 
 export default DemoComp1;
